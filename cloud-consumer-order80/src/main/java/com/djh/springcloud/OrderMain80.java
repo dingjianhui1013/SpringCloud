4ee3c482fc7ef86663669f0cdc9530bdfd3f7ffd
djh <djh_it@163.com>
2022-02-17 14:56:13 +0800

    将支付端80 注册入 服务中心

--- cloud-consumer-order80/src/main/java/com/djh/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/djh/springcloud/OrderMain80.java
@@ -2,8 +2,11 @@ package com.djh.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
